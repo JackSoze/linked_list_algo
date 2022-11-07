93c0763432dd017b8bd9cd6948e9a645c8657f7f
jacksoze <kamonyojack@gmail.com>
2022-11-07 12:10:06 +0300

    Create head and tail methods

--- lib/linked_lists.rb
+++ lib/linked_lists.rb
@@ -30,19 +30,35 @@ class LinkedList
    self.size +=1
   end
 
+  def head
+    @head.to_s
+  end
+
+  def tail
+    @tail.to_s
+  end
 
 end
 
 class Node
-  attr_accessor :value, :next
+  attr_accessor :value, :next, :to_s
 
   def initialize(value)
     @value = value
     @next = nil
   end
+
+  def to_s
+    check = -> {if @next == nil then "value nil" else @next.value end}
+    puts "[value: #{value}|next: #{check.call}]"
+  end
 end
 
 
 my_list = LinkedList.new
-my_list.pre_pend('kimy')
+my_list.pre_pend('kim')
+my_list.append('jack')
+my_list.append('miriti')
 puts my_list.size
+puts my_list.head
+puts my_list.tail
